6aa427a858dd57c2b22cbeab0e49c85fba45aad7
Toexport <chibizhangpeng@163.com>
2019-03-12 08:13:51 +0800

    新的项目

---
+++ Bohan/js/Forgotpassword.js
@@ -0,0 +1,9 @@
+
+// 不需要sock的接口http://www.bohanserver.top:8088/webservice.asmx；
+<script>
+function ForgotPassword() {
+	x = document.getElementById("Forgotpass");
+	x.innerHTML =  "http://www.bohan-smartec.com/";
+}
+
+</script>
