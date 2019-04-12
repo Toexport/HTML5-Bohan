54e79285c2eae044618e037246bddd8530dfe694
Toexport <chibizhangpeng@163.com>
2019-04-12 18:28:13 +0800

    修改细节

--- Bohan/js/Registered/Registered.js
+++ Bohan/js/Registered/Registered.js
@@ -47,32 +47,35 @@ function sendmessage(obj,second){
         url:'http://www.bohanserver.top:8088/webservice.asmx/GetRegisterCode',
         data : {"mobileNum":name,"flag":0},
         //设置请求方法
-        //设置请求方法
         type : 'GET',
         //设置数据类型
-         dataType: "xml",
-         timeout: 1000,      //设定超时
-         cache: false,       //禁用缓存
+        dataType: 'json',
         success : function(data) {
-        	console.log(data)
-            if (data){
-                if (data.code == "0") { //判断返回值，这里根据的业务内容可做调整
-                        setTimeout(function () {//做延时以便显示登录状态值
-                           showMsg("正在登录中...");
-                           showMsg(data.message);
-//                         console.log(data);
-//                         window.location.href =  url;//指向登录的页面地址
-//                        window.location.href = "../list/list .js";
-                       },100)
-                    } else {
-                        showMsg(data.message);//显示登录失败的原因
-//                      window.location.href = "../Login/Login.js";
-                        return false;
-                    }
-                }
+        	data = $.xml2json(data);// xml转json
+        	var strData = data;
+        	var str = strData.substring(); //获取指定的字符
+        	console.log(str);
+        var obj = JSON.parse(str);
+        	var code = obj.statusCode;
+        if (obj.statusCode == 0) { //判断返回值，这里根据的业务内容可做调整
+           setTimeout(function () {//做延时以便显示登录状态值
+              showMsg("正在登录中...");
+              showMsg(obj.message);
+              alert(obj.message);
+              console.log(obj.message);
+              var content = obj.content;// Token
+              console.log(content);
+//             window.location.href =  url;//指向登录的页面地址
+             },100);
+             }else {
+                 showMsg(obj.message);//显示登录失败的原因
+                 alert(obj.message);
+                 return false;
+               }
             }, 
             error : function(data){
-                showMsg(data.message);  
+            	    console.log("Fail");
+                showMsg(obj.message);  
             }
     });
  }
